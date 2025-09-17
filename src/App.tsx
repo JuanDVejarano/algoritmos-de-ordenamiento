4666afb1a35163e859de73d7317f735861283dd1
JuanDVejarano <juanjdv_200@hotmail.com>
2025-09-17 02:45:46 -0500

    Limpieza de variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.scss";
 import InsertionSort from "./Algorithms/InsertionSort/InsertionSort";
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <InsertionSort />
